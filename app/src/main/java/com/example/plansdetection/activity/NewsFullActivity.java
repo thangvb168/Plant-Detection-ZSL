55469905ea297e8e3e60eb517bb8716148dd7e96
tphuyen <trinhhuyen.tph@gmail.com>
2024-04-17 22:59:01 +0700

    Done fix layout News

--- app/src/main/java/com/example/plansdetection/activity/NewsFullActivity.java
+++ app/src/main/java/com/example/plansdetection/activity/NewsFullActivity.java
@@ -6,25 +6,32 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 import com.example.plansdetection.R;
 
 public class NewsFullActivity extends AppCompatActivity {
 
+    ImageView arrowBack;
     WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_full);
 
+        arrowBack = findViewById(R.id.arrowBack);
         String url = getIntent().getStringExtra("url");
         webView = findViewById(R.id.web_view);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
+        setupBackBtn();
     }
 
+    private void setupBackBtn() {
+        arrowBack.setOnClickListener(v -> onBackPressed());
+    }
     @Override
     public void onBackPressed() {
         if(webView.canGoBack())
